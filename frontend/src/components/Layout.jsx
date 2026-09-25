80989a18d63834d036b605c5b46780e9f98d2a24
Carlos Pereira <carlosguilherme1010@gmail.com>
2026-09-25 10:16:53 +0000

    Ajuste - ajustes finais no front-end: remove logo e nome do cabeçalho

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,17 +1,10 @@
-import { Link, NavLink, Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 
 export default function Layout() {
   return (
     <div className="app">
       <header className="topbar">
         <div className="container topbar__content">
-          <Link to="/apolices" className="brand">
-            <img src="/favicon.svg" alt="" width="32" height="32" />
-            <div>
-              <strong>Coris Seguros</strong>
-              <span>Gestão de Apólices</span>
-            </div>
-          </Link>
           <nav className="menu">
             <NavLink to="/apolices" className="menu__item">
               Apólices
